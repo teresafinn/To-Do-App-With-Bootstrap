c7a2ae9e3f57a9a7d473103e3a27d884d0a9ffcf
Teresa Finn <teresa.finn@gmail.com>
2015-04-29 23:05:10 -0600

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Todo.create(title: 'Whole Foods', notes: 'spring mix, mushrooms, carrots, raspberries')
+Todo.create(title: 'Wash Car', notes: 'clean out trunk')
+Todo.create(title: 'Travel Research', notes: 'check out airbnb for places to stay near CO national parks')
+Todo.create(title: 'Costco', notes: 'frozen strawberries, fresh fruit')
+Todo.create(title: 'Plane Tickets', notes: 'book plane tickets to CA for Rylee\'s play')
+Todo.create(title: 'Spring Cleaning', notes: 'clean out junk boxes!')
