2817329c3d36721a3edaf454bff99e9f74cecc00
Teresa Finn <teresa.finn@gmail.com>
2015-05-01 21:55:01 -0600

    Override button listing for edit and new actions, and pasted in css fix to fix font display bug in chrome browser

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,28 +1,20 @@
 class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :edit, :update, :destroy]
 
-  # GET /todos
-  # GET /todos.json
   def index
     @todos = Todo.all
   end
 
-  # GET /todos/1
-  # GET /todos/1.json
   def show
   end
 
-  # GET /todos/new
   def new
     @todo = Todo.new
   end
 
-  # GET /todos/1/edit
   def edit
   end
 
-  # POST /todos
-  # POST /todos.json
   def create
     @todo = Todo.new(todo_params)
 
@@ -37,8 +29,6 @@ class TodosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /todos/1
-  # PATCH/PUT /todos/1.json
   def update
     respond_to do |format|
       if @todo.update(todo_params)
@@ -51,8 +41,6 @@ class TodosController < ApplicationController
     end
   end
 
-  # DELETE /todos/1
-  # DELETE /todos/1.json
   def destroy
     @todo.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class TodosController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_todo
       @todo = Todo.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def todo_params
       params.require(:todo).permit(:title, :notes)
     end
